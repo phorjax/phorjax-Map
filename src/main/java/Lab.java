96e6d1839b07bc6a68179210a8340a90955bd1e7
phorjax <joseph.12082@gmail.com>
2023-06-29 20:59:44 +0000

    done

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,5 +1,6 @@
 
 import java.util.Map;
+import java.util.HashMap;
 
 /**
  * Maps are key/value pairs. They are used in situations where we have some value that we'd like
@@ -17,7 +18,7 @@ public class Lab {
 
     /**
      * Create and instantiate a collection that implements that Map interface, like
-     * TreeSet or HashSet. This will be a Map that maps Integer keys to String values.
+     * TreeSet or HashSet. This will be a Map that maps Integer keys to String values. 
      * For example, 1:cat, 2:dog, 3:rat, etc.
      *
      * @return a new Map instantiated using Integer keys and String values.
@@ -26,7 +27,8 @@ public class Lab {
      * so here Integer is a wrapper for int.
      */
     public Map<Integer, String> createMap(){
-        return null;
+        Map<Integer, String> map = new HashMap<Integer, String>();
+        return map;
     }
 
     /**
@@ -36,7 +38,7 @@ public class Lab {
      */
     public int getSize(Map<Integer,String> map){
 //        return total number of key/value pairs in the map
-        return 0;
+        return map.size();
     }
 
     /**
@@ -47,6 +49,8 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void addKeyValuePair(Map<Integer,String> map, int key, String value){
+        map.put(key, value);
+
     }
 
     /**
@@ -56,7 +60,8 @@ public class Lab {
      * @return the value associated with key in map.
      */
     public String getValueFromKey(Map<Integer, String> map, int key){
-        return "";
+
+        return map.get(key);
     }
 
     /**
@@ -66,6 +71,7 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void removeKeyValuePair(Map<Integer, String> map, int key){
+        map.remove(key);
     }
 
     /**
@@ -76,6 +82,7 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void overwriteValue(Map<Integer, String> map, int key, String value){
+         map.replace(key, value);
 
     }
 }
